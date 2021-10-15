c0f2f17d8e3d021838b05cbfc30427695ff77f16
UchechiUcheAjike <ajikeuchechiuche@gmail.com>
2021-10-15 17:18:30 -0600

    Update jumper.py
    Added comments to the jumper file

--- jumper/game/jumper.py
+++ jumper/game/jumper.py
@@ -1,4 +1,13 @@
+""" The random function being called """
 import random
+
+"""" The jumper class
+The __init__ function gives the self.lives an equivalent of 4
+
+The has_lives function has an if and else statements that 
+return a true boolean if the __init__ function equivalent 
+is greater than zero. If less than zero, the else statement 
+returns false"""
 class Jumper:
 
     def __init__(self):
